6933e1ef4b16ec9ec1bbca460eb0d82a53514eba
unknown <darshitpatel843@gmail.com>
2018-05-08 13:42:28 +0530

    add commit

---
+++ darshit/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Darshit' },
+  { id: 2, name: 'Dhruvik' },
+  { id: 3, name: 'Mihir' },
+  { id: 4, name: 'Akshay' },
+  { id: 5, name: 'Ghansyam' },
+  { id: 6, name: 'Dharmik' },
+  { id: 7, name: 'Kaushik' },
+  { id: 8, name: 'Milin' },
+  { id: 9, name: 'Himanshu' },
+  { id: 10, name: 'Abhishek' }
+];
